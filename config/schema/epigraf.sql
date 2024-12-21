2e5ae82d44865987a1525e53eaef3fe1b0578468
Jakob Jünger <jakob.juenger@adwmainz.de>
2024-12-21 17:07:32 +0100

    Update schema, add version.txt

--- config/schema/epigraf.sql
+++ config/schema/epigraf.sql
@@ -1,183 +1,133 @@
-CREATE TABLE IF NOT EXISTS `databanks`
-(
-    `id`          int(11)      NOT NULL AUTO_INCREMENT,
-    `created`     datetime                                                    DEFAULT NULL,
-    `modified`    datetime                                                    DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-    `name`        varchar(200) NOT NULL COMMENT 'Name der Datenbank',
-    `version`     varchar(5)   NOT NULL,
-    `description` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `published`   tinyint(4)                                                  DEFAULT NULL,
-    `iriprovider` tinyint(4)   NOT NULL                                       DEFAULT 0 COMMENT 'Referenzdatenbank ja/nein',
-    `iriprefix`   varchar(50)                                                 DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    KEY `name` (`name`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+CREATE TABLE IF NOT EXISTS `databanks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `name` varchar(200) NOT NULL,
+  `version` varchar(5) NOT NULL,
+  `category` varchar(200) DEFAULT NULL,
+  `description` mediumtext DEFAULT NULL,
+  `published` tinyint(4) DEFAULT NULL,
+  `iriprefix` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `docs`
-(
-    `id`         int(11)     NOT NULL AUTO_INCREMENT,
-    `deleted`    tinyint(4)  NOT NULL DEFAULT 0,
-    `version_id` int(11)              DEFAULT NULL,
-    `created`    datetime             DEFAULT NULL,
-    `modified`   datetime             DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-    `published`  tinyint(4)  NOT NULL DEFAULT 0,
-    `segment`    char(10)             DEFAULT 'wiki',
-    `sortkey`    varchar(50)          DEFAULT '',
-    `name`       varchar(200)         DEFAULT NULL,
-    `category`   varchar(300)         DEFAULT NULL,
-    `content`    mediumtext                 DEFAULT NULL,
-    `format`     varchar(50) NOT NULL DEFAULT 'html',
-    `norm_iri`   varchar(50)          DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    KEY `norm_iri` (`norm_iri`) USING BTREE
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+CREATE TABLE IF NOT EXISTS `docs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` tinyint(4) NOT NULL DEFAULT 0,
+  `version_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `published` tinyint(4) NOT NULL DEFAULT 0,
+  `menu` tinyint(4) NOT NULL DEFAULT 1,
+  `segment` char(10) DEFAULT 'wiki',
+  `sortkey` varchar(50) DEFAULT '',
+  `name` varchar(200) DEFAULT NULL,
+  `category` varchar(300) DEFAULT NULL,
+  `content` mediumtext DEFAULT NULL,
+  `format` varchar(50) NOT NULL DEFAULT 'html',
+  `norm_iri` varchar(1500) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `norm_iri` (`norm_iri`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `files`
-(
-    `id`          int(11)                                          NOT NULL AUTO_INCREMENT,
-    `deleted`     int(11)                                          NOT NULL DEFAULT 0,
-    `published`   int(11)                                          NOT NULL DEFAULT 0,
-    `created`     datetime                                                  DEFAULT NULL,
-    `modified`    datetime                                                  DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-    `name`        varchar(500) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-    `description` mediumtext COLLATE utf8mb4_unicode_ci                              DEFAULT NULL,
-    `type`        varchar(100) CHARACTER SET utf8 COLLATE utf8_bin          DEFAULT NULL,
-    `size`        int(11)                                                   DEFAULT NULL,
-    `root`        varchar(100) CHARACTER SET utf8 COLLATE utf8_bin          DEFAULT 'root',
-    `path`        varchar(500) CHARACTER SET utf8 COLLATE utf8_bin          DEFAULT NULL,
-    `isfolder`    tinyint(4)                                       NOT NULL DEFAULT 0,
-    PRIMARY KEY (`id`),
-    KEY `published` (`published`)
-) ENGINE = MyISAM
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+CREATE TABLE IF NOT EXISTS `files` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` int(11) NOT NULL DEFAULT 0,
+  `published` int(11) NOT NULL DEFAULT 0,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `name` varchar(500) NOT NULL,
+  `description` text DEFAULT NULL,
+  `config` longtext DEFAULT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  `size` int(11) DEFAULT NULL,
+  `root` varchar(100) DEFAULT 'root',
+  `path` varchar(500) DEFAULT NULL,
+  `isfolder` tinyint(4) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `published` (`published`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `jobs`
-(
-    `id`          int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `created`     datetime                  DEFAULT NULL,
-    `modified`    datetime                  DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-    `typ`         varchar(100)     NOT NULL COMMENT 'Typ',
-    `status`      varchar(50)      NOT NULL COMMENT 'Status',
-    `progress`    int(11)          NOT NULL DEFAULT 0,
-    `progressmax` int(11)          NOT NULL DEFAULT 0,
-    `config`      mediumtext                      DEFAULT NULL COMMENT 'Optionen',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+CREATE TABLE IF NOT EXISTS `jobs` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `typ` varchar(100) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `progress` int(11) NOT NULL DEFAULT 0,
+  `progressmax` int(11) NOT NULL DEFAULT 0,
+  `config` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `permissions`
-(
-    `id`                 int(11) NOT NULL AUTO_INCREMENT,
-    `created`            datetime     DEFAULT NULL,
-    `modified`           datetime     DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-    `user_id`            int(11)      DEFAULT NULL,
-    `user_session`       int(11)      DEFAULT NULL,
-    `user_role`          varchar(50)  DEFAULT NULL,
-    `user_request`       varchar(50)  DEFAULT NULL,
-    `entity_type`        varchar(50)  DEFAULT NULL,
-    `entity_name`        varchar(50)  DEFAULT NULL,
-    `entity_id`          int(11)      DEFAULT NULL,
-    `permission_type`    varchar(200) DEFAULT NULL COMMENT 'Name der Berechtigung',
-    `permission_name`    varchar(200) DEFAULT NULL,
-    `permission_expires` datetime     DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    KEY `user_id` (`user_id`),
-    KEY `entity_type_entity_id_permission_type` (`entity_type`, `entity_id`, `permission_type`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `user_session` int(11) DEFAULT NULL,
+  `user_role` varchar(50) DEFAULT NULL,
+  `user_request` varchar(50) DEFAULT NULL,
+  `entity_type` varchar(50) DEFAULT NULL,
+  `entity_name` varchar(50) DEFAULT NULL,
+  `entity_id` int(11) DEFAULT NULL,
+  `permission_type` varchar(200) DEFAULT NULL,
+  `permission_name` varchar(200) DEFAULT NULL,
+  `permission_expires` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `entity_type_entity_id_permission_type` (`entity_type`,`entity_id`,`permission_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `pipelines` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`deleted` TINYINT(4) NOT NULL DEFAULT '0',
-	`published` TINYINT(4) NULL DEFAULT NULL,
-	`version_id` INT(11) NULL DEFAULT NULL,
-	`created` DATETIME NULL DEFAULT NULL,
-	`modified` DATETIME NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-	`created_by` INT(11) NULL DEFAULT NULL,
-	`modified_by` INT(11) NULL DEFAULT NULL,
-	`name` VARCHAR(200) NOT NULL COMMENT 'Name der Pipeline' COLLATE 'utf8mb4_general_ci',
-	`norm_iri` VARCHAR(1500) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`description` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	`tasks` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	PRIMARY KEY (`id`) USING BTREE
-)
-DEFAULT CHARSET = utf8mb4
-COLLATE='utf8mb4_general_ci'
-ENGINE=InnoDB;
-
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` tinyint(4) NOT NULL DEFAULT 0,
+  `published` tinyint(4) DEFAULT NULL,
+  `version_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `name` varchar(200) NOT NULL,
+  `norm_iri` varchar(1500) NOT NULL,
+  `description` text DEFAULT NULL,
+  `tasks` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `users` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`created` DATETIME NULL DEFAULT NULL,
-	`modified` DATETIME NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-	`created_by` INT(11) NULL DEFAULT NULL,
-	`modified_by` INT(11) NULL DEFAULT NULL,
-	`lastaction` DATETIME NULL DEFAULT NULL,
-	`username` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`password` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`email` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	`name` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	`acronym` VARCHAR(10) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	`norm_iri` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	`contact` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	`accesstoken` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	`role` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`databank_id` INT(11) NULL DEFAULT NULL,
-	`pipeline_article_id` INT(11) NULL DEFAULT NULL,
-	`pipeline_book_id` INT(11) NULL DEFAULT NULL,
-	`settings` MEDIUMTEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	PRIMARY KEY (`id`) USING BTREE,
-	UNIQUE INDEX `Username` (`username`) USING BTREE,
-	UNIQUE INDEX `iri` (`norm_iri`) USING BTREE
-)
-ENGINE=InnoDB
-DEFAULT CHARSET = utf8mb4
-COLLATE='utf8mb4_general_ci';
-
-
-ALTER TABLE `databanks` DROP COLUMN `iriprovider`;
-UPDATE databanks SET published = 1 WHERE name ='epi_public';
-
-ALTER TABLE `databanks`
-    ADD COLUMN `created_by` int(11) DEFAULT NULL AFTER modified,
-   ADD COLUMN `modified_by` int(11) DEFAULT NULL AFTER `created_by`;
-
-ALTER TABLE `docs`
-    ADD COLUMN `created_by` int(11) DEFAULT NULL AFTER modified,
-   ADD COLUMN `modified_by` int(11) DEFAULT NULL AFTER `created_by`;
-
-ALTER TABLE `files`
-    ADD COLUMN `created_by` int(11) DEFAULT NULL AFTER modified,
-   ADD COLUMN `modified_by` int(11) DEFAULT NULL AFTER `created_by`;
-
-ALTER TABLE `jobs`
-    ADD COLUMN `created_by` int(11) DEFAULT NULL AFTER modified,
-   ADD COLUMN `modified_by` int(11) DEFAULT NULL AFTER `created_by`;
-
-ALTER TABLE `permissions`
-    ADD COLUMN `created_by` int(11) DEFAULT NULL AFTER modified,
-   ADD COLUMN `modified_by` int(11) DEFAULT NULL AFTER `created_by`;
-
-
-ALTER TABLE `users`
-    ADD COLUMN `created_by` int(11) DEFAULT NULL AFTER modified,
-   ADD COLUMN `modified_by` int(11) DEFAULT NULL AFTER `created_by`;
-
-    ALTER TABLE `docs`
-        CHANGE COLUMN `norm_iri` `norm_iri` VARCHAR(1500) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci' AFTER `format`;
-
-    ALTER TABLE `docs`
-        ADD COLUMN `menu` TINYINT(4) NOT NULL DEFAULT 1 AFTER `published`;
-
-    ALTER TABLE `files`
-        ADD COLUMN `config` LONGTEXT NULL AFTER `description`;
-
-ALTER TABLE `databanks`
-	ADD COLUMN `category` VARCHAR(200) NULL AFTER `version`;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `lastaction` datetime DEFAULT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `acronym` varchar(10) DEFAULT NULL,
+  `iri` varchar(255) DEFAULT NULL,
+  `norm_iri` varchar(255) DEFAULT NULL,
+  `contact` text DEFAULT NULL,
+  `accesstoken` varchar(255) DEFAULT NULL,
+  `role` varchar(50) NOT NULL,
+  `databank_id` int(11) DEFAULT NULL,
+  `pipeline_article_id` int(11) DEFAULT NULL,
+  `pipeline_book_id` int(11) DEFAULT NULL,
+  `settings` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `iri` (`norm_iri`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
